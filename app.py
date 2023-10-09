28a756a89795945ba701b22a93db1669715b16b6
William Morgan <williammorgan864@gmail.com>
2023-10-09 18:21:26 +0100

    set up envron to run

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "<h1>Hello</h1>"
+    return "<h1>Hello World!</h1>"
