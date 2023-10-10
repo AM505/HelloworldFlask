2b312b66ebe91b169916a5b11703eb5a70cbbd34
William Morgan <williammorgan864@gmail.com>
2023-10-10 23:13:30 +0100

    added

--- hello_world/models.py
+++ hello_world/models.py
@@ -1,4 +1,5 @@
 from hello_world import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,6 +18,12 @@ class User(db.Model):
     username = db.Column(db.String(25), unique=True, nullable=False)
     password_hash = db.Column(db.String(355))
     is_admin = db.Column(db.Boolean)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
